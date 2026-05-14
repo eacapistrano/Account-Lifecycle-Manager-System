5085b798a75fc2d9a0bf109ee33a1fea14a22b15
eacapistrano <eacapistrano@ceu.edu.ph>
2026-05-14 14:20:13 +0800

    Google Login First Update

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,15 +11,33 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+     Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+
+            // Basic Info
+            $table->string('name')->nullable();
+            $table->string('user_id')->nullable();
             $table->string('email')->unique();
+
+            // Social Login
+            $table->string('google_id')->nullable()->unique();
+            $table->string('provider')->nullable();
+
+            // User Metadata
+            $table->string('join_date')->nullable();
+            $table->string('last_login')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->string('status')->nullable();
+            $table->string('role_name')->nullable();
+            $table->string('avatar')->nullable();
+            $table->string('position')->nullable();
+
+            // Authentication
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->rememberToken();
             $table->timestamps();
-        });
+       });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
